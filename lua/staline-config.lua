a7b1ab025aeb10b95a72368c7f7f770235af0b17
Diego Lima <diegohblima@proton.me>
2023-05-04 18:51:50 -0300

    fwefw

--- lua/staline-config.lua
+++ lua/staline-config.lua
@@ -4,7 +4,12 @@ require "staline".setup {
 		mid = {},
 		right = {'file_name', 'line_column' }
 	},
-	mode_colors = catppuccin,
+	mode_colors = {
+		i = "#a6d189",
+		n = "#85c1dc",
+		c = "#e78284",
+		v = "#f4b8e4",
+	},
 	defaults = {
 		true_colors = true,
 		line_column = " %l/%L :%c  ",
