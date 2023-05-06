06addb874aaf3f6174ce9d3819fe5169f70e7f83
Diego Lima <diegohblima@proton.me>
2023-05-06 08:55:54 -0300

    color staline

--- lua/staline-config.lua
+++ lua/staline-config.lua
@@ -1,18 +1,18 @@
 require "staline".setup {
 	sections = {
-		left = { '  ', 'mode', ' ', 'branch', ' ', 'lsp' },
+		left = { '  ', 'mode', ' ', 'branch', '', 'lsp' },
 		mid = {},
 		right = {'file_name', 'line_column' }
 	},
 	mode_colors = {
-		i = "#7daea3",
-		n = "#a9b665",
-		c = "#e78a4e",
-		v = "#d8a657",
+		i = "#a9b665",
+		n = "#a89984",
+		c = "#89b482",
+		v = "#ea6962",
 	},
 	defaults = {
 		true_colors = true,
-		line_column = "%l/%L :%c  並%p%%",
-		branch_symbol = " "
+		line_column = " %l/%L :%c  並%p%% ",
+		branch_symbol = " "
 	}
 }
