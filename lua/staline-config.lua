b23f88ac2cb940ede9424c5455aa95ad41bd551d
Diego Lima <diegohblima@proton.me>
2023-05-05 23:23:06 -0300

    config staline

--- lua/staline-config.lua
+++ lua/staline-config.lua
@@ -4,10 +4,15 @@ require "staline".setup {
 		mid = {},
 		right = {'file_name', 'line_column' }
 	},
-	mode_colors = gruvbox,
+	mode_colors = {
+		i = "#7daea3",
+		n = "#a9b665",
+		c = "#e78a4e",
+		v = "#d8a657",
+	},
 	defaults = {
 		true_colors = true,
-		line_column = " %l/%L :%c  ",
+		line_column = "%l/%L :%c  並%p%%",
 		branch_symbol = " "
 	}
 }
