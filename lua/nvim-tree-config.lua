356ecaacec72a600605bcb269d5c8c269644751c
Diego Lima <diegold@tutanota.com>
2023-05-11 21:43:30 -0300

    config nvim tree

--- lua/nvim-tree-config.lua
+++ lua/nvim-tree-config.lua
@@ -1,58 +1,11 @@
-require'nvim-tree'.setup{
+require("nvim-tree").setup {
   sync_root_with_cwd = true,
   respect_buf_cwd = true,
- -- update_focused_file = {
-  --  enable = true,
---    update_root = true
- -- },
   view = {
     adaptive_size = true,
   },
   filters = {
     dotfiles = true,
-  },
-  renderer = {
-    indent_markers = {
-      enable = true,
-      icons = {
-        corner = "└ ",
-        edge = "│ ",
-        item = "│ ",
-        none = "  ",
-      },
-    },
-    icons = {
-      webdev_colors = true,
-      show = {
-        file = true,
-        folder = true,
-        folder_arrow = false,
-        git = true
-      },
-      glyphs = {
-        default = "",
-        symlink = "",
-        folder = {
-          arrow_closed = "",
-          arrow_open = "",
-          default = "",
-          open = "",
-          empty = "",
-          empty_open = "",
-          symlink = "",
-          symlink_open = "",
-        },
-        git = {
-          unstaged = "", -- 
-          staged = "",
-          unmerged = "",
-          renamed = "➜",
-          untracked = "",
-          deleted = "",
-          ignored = "◌",
-        },
-      },
-    },
   },
   update_focused_file = {
     enable = true,
@@ -68,5 +21,30 @@ require'nvim-tree'.setup{
       warning = "",
       error = "",
     },
+  },
+  renderer = {
+    root_folder_label = false,
+    indent_markers = {
+      enable = true
+    },
+    icons = {
+      show = {
+        file = true,
+        folder = true,
+        folder_arrow = false,
+        git = true
+      },
+      glyphs = {
+        git = {
+          unstaged = "",
+          staged = "",
+          unmerged = "",
+          renamed = "➜",
+          untracked = "",
+          deleted = "",
+          ignored = "◌",
+        },
+      }
+    }
   }
 }
