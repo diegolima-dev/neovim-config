b565eca8a6329365c98fe85c82c61ddd30d77bd1
Diego Lima <diegold@tutanota.com>
2023-05-14 11:17:13 -0300

    protected call

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -17,6 +17,12 @@ vim.cmd [[
   augroup end
 ]]
 
+-- Use a protected call so we don't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
 local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
