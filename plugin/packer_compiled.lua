97ee206e2cc9950c0ab70a301e2f9e193882a0b8
Diego Lima <diegold@tutanota.com>
2023-05-14 21:18:36 -0300

    telescope config

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -163,11 +163,6 @@ _G.packer_plugins = {
     path = "/home/diego/.local/share/nvim/site/pack/packer/start/nui.nvim",
     url = "https://github.com/MunifTanjim/nui.nvim"
   },
-  ["null-ls.nvim"] = {
-    loaded = true,
-    path = "/home/diego/.local/share/nvim/site/pack/packer/start/null-ls.nvim",
-    url = "https://github.com/jose-elias-alvarez/null-ls.nvim"
-  },
   ["nvim-autopairs"] = {
     loaded = true,
     path = "/home/diego/.local/share/nvim/site/pack/packer/start/nvim-autopairs",
